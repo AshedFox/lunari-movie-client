e38611c6a1e8e9f43fdb24e2bd7cc221ab4c0d5b
AshedFox <a.mahn@mail.ru>
2025-02-20 16:00:52 +0300

    add auth cookies set/reset functions

---
+++ src/lib/auth/cookie.ts
@@ -0,0 +1,47 @@
+import 'server-only';
+
+import { cookies } from 'next/headers';
+import {
+  ACCESS_COOKIE_KEY,
+  REFRESH_COOKIE_KEY,
+  USER_COOKIE_KEY,
+} from './constants';
+import { UserProfileFragment } from '@lib/graphql/generated/graphql';
+import { ResponseCookies } from 'next/dist/compiled/@edge-runtime/cookies';
+
+export async function setAuthCookies(
+  accessToken: string,
+  refreshToken: string,
+  user: UserProfileFragment,
+  cookiesStore?: ResponseCookies,
+) {
+  const cookieStore = cookiesStore || (await cookies());
+  cookieStore.set(ACCESS_COOKIE_KEY, accessToken, {
+    sameSite: 'strict',
+    path: '/',
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    maxAge: 15 * 60,
+  });
+  cookieStore.set(REFRESH_COOKIE_KEY, refreshToken, {
+    sameSite: 'strict',
+    path: '/',
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60,
+  });
+  cookieStore.set(USER_COOKIE_KEY, JSON.stringify(user), {
+    sameSite: 'strict',
+    path: '/',
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+    maxAge: 15 * 60,
+  });
+}
+
+export async function resetAuthCookies(cookiesStore?: ResponseCookies) {
+  const cookieStore = cookiesStore || (await cookies());
+  cookieStore.delete(ACCESS_COOKIE_KEY);
+  cookieStore.delete(REFRESH_COOKIE_KEY);
+  cookieStore.delete(USER_COOKIE_KEY);
+}
