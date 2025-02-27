147d4ec61c4b5966bc7f123a66be6f8364fe2653
AshedFox <a.mahn@mail.ru>
2025-02-28 01:39:21 +0300

    fix: remove h-screen from login and sign up pages

--- src/app/(guest)/login/page.tsx
+++ src/app/(guest)/login/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 const Page = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <main className="w-full m-4 md:m-0 md:w-98 lg:w-114 xl:w-140 2xl:w-156">
         <LoginForm />
       </main>
