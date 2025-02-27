a1853fdd04444f8aa023bfada81e79856d5b019b
AshedFox <a.mahn@mail.ru>
2025-02-28 01:20:40 +0300

    add cache to user dal

--- src/lib/auth/user-dal.ts
+++ src/lib/auth/user-dal.ts
@@ -3,8 +3,9 @@ import 'server-only';
 import { cookies } from 'next/headers';
 import { USER_COOKIE_KEY } from '@lib/auth/constants';
 import { UserProfileFragment } from '@lib/graphql/generated/graphql';
+import { cache } from 'react';
 
-export async function getUser(): Promise<UserProfileFragment | null> {
+export const getUser = cache(async (): Promise<UserProfileFragment | null> => {
   const userValue = (await cookies()).get(USER_COOKIE_KEY)?.value;
   return userValue ? JSON.parse(userValue) : null;
-}
+});
