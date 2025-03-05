ea261565591bd2dfa8354d59769e6314673cd152
AshedFox <a.mahn@mail.ru>
2025-03-05 18:43:17 +0300

    add skip auth option to auth links

--- src/lib/apollo/auth-link.ts
+++ src/lib/apollo/auth-link.ts
@@ -3,6 +3,10 @@
 import { setContext } from '@apollo/client/link/context';
 
 export const authLink = setContext(async (_, context) => {
+  if (context.skipAuth) {
+    return context;
+  }
+
   const res = await fetch('/api/auth/token', {
     method: 'GET',
     credentials: 'include',
--- src/lib/apollo/rsc-auth-link.ts
+++ src/lib/apollo/rsc-auth-link.ts
@@ -2,23 +2,27 @@ import 'server-only';
 
 import { setContext } from '@apollo/client/link/context';
 import { cookies, headers } from 'next/headers';
-import { ACCESS_COOKIE_KEY } from '@lib/auth/constants';
+import { ACCESS_COOKIE_KEY, REFRESH_COOKIE_KEY } from '@lib/auth/constants';
 
 export const authLink = setContext(async (_, context) => {
+  if (context.skipAuth) {
+    return context;
+  }
+
   let token = (await cookies()).get(ACCESS_COOKIE_KEY)?.value;
 
   if (!token) {
     const refrehsToken = (await cookies()).get(REFRESH_COOKIE_KEY)?.value;
 
     if (refrehsToken) {
-    const origin = (await headers()).get('x-origin');
+      const origin = (await headers()).get('x-origin');
 
-    if (origin) {
-      await fetch(`${origin}/api/auth/refresh`, {
-        method: 'POST',
-        credentials: 'include',
-      });
-      token = (await cookies()).get(ACCESS_COOKIE_KEY)?.value;
+      if (origin) {
+        await fetch(`${origin}/api/auth/refresh`, {
+          method: 'POST',
+          credentials: 'include',
+        });
+        token = (await cookies()).get(ACCESS_COOKIE_KEY)?.value;
       }
     }
   }
