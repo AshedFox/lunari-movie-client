de8a1ea30eb4442d1a2292d48e917f45e8209290
AshedFox <a.mahn@mail.ru>
2025-03-11 15:24:39 +0300

    redesign card components

--- src/components/film/card/FilmCard.tsx
+++ src/components/film/card/FilmCard.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { ScrollBar, ScrollArea } from '@components/ui/scroll-area';
 import { FilmListItemFragment } from '@lib/graphql/generated/graphql';
 import { ISODateToLocale } from '@lib/utils/format';
 import { Star } from 'lucide-react';
@@ -12,51 +11,94 @@ type Props = {
 };
 
 const FilmCard = ({ film }: Props) => {
-  const tags = [...film.genres, ...film.countries, ...film.studios];
+  const releaseDate = ISODateToLocale(film.releaseDate);
 
   return (
-    <article className="flex flex-col overflow-hidden rounded-lg border h-full divide-y">
-      <div className="relative w-full aspect-video">
-        {film.cover?.url && (
+    <article className="bg-card text-card-foreground rounded-xl shadow-md overflow-hidden border flex flex-col">
+      {/* Main info */}
+      <div className="relative aspect-video">
+        {/* Cover */}
+        {film.cover && (
           <Image
-            src={film.cover.url}
-            fill
             className="object-cover"
-            alt={'cover'}
+            fill
+            src={film.cover?.url}
+            alt={`Cover ${film.title}`}
           />
         )}
-        <div className="absolute top-0 left-0 bg-primary text-sm font-bold rounded-br-md px-3 py-1 text-primary-foreground drop-shadow-lg">
-          {film.ageRestriction}
-        </div>
-        <div className="absolute bottom-0 left-0 bg-chart-1 text-sm font-bold rounded-tr-md px-3 py-1 text-primary-foreground drop-shadow-lg">
-          film
-        </div>
-        <div className="absolute bottom-0 right-0 bg-chart-3 text-sm font-bold rounded-tl-md px-3 py-1 text-primary-foreground drop-shadow-lg flex gap-2 items-center">
-          {film.rating || '-'} <Star className="size-4 fill-current" />
-        </div>
-      </div>
-      <div className="p-4 space-y-2 flex flex-col flex-1">
-        <Link className="w-fit" href={`/films/${film.id}`}>
-          <h2 className="text-xl font-semibold truncate">{film.title}</h2>
-        </Link>
-        <div className="line-clamp-4 text-sm ">{film.description}</div>
-        <div className="font-bold text-sm mt-auto">
-          {ISODateToLocale(film.releaseDate)}
-        </div>
-      </div>
-      <ScrollArea className="w-full  scroll-p-3">
-        <div className="flex space-x-1.5 p-3 w-max h-12">
-          {tags.map((tag) => (
-            <div
-              key={tag.id}
-              className="bg-secondary text-sm rounded py-0.5 px-2 shrink-0 text-secondary-foreground snap-start"
-            >
-              {tag.name}
+        <div className="absolute inset-0 bg-gradient-to-b via-transparent via-45% to-card" />
+
+        {/* Information over the cover */}
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between p-6">
+          <div className="flex justify-between items-start">
+            {/* Rating */}
+            <div className="flex items-center gap-1 px-2 py-1 bg-yellow-500/40 text-primary-foreground backdrop-blur rounded-md font-semibold text-sm">
+              <Star className="text-yellow-500" fill="currentColor" size={16} />
+              {film.rating === 0 ? '-' : film.rating.toFixed(1)}
             </div>
-          ))}
+
+            {/* Age restriction */}
+            <div className="text-sm font-semibold bg-red-600/60 text-primary-foreground rounded-md backdrop-blur px-2 py-1">
+              {film.ageRestriction}
+            </div>
+          </div>
+
+          <div>
+            {/* Title */}
+            <h2 className="text-2xl font-bold w-fit">
+              <Link href={`/films/${film.id}`}>{film.title}</Link>
+            </h2>
+
+            {/* Release date */}
+            {releaseDate && (
+              <div className="text-muted-foreground text-sm">{releaseDate}</div>
+            )}
+          </div>
         </div>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
+      </div>
+
+      {/* Other info*/}
+      <div className="px-6 py-4 space-y-4 flex-1 flex flex-col">
+        {/* Description */}
+        <div className="flex-1">
+          <p className="text-muted-foreground text-sm line-clamp-4">
+            {film.description}
+          </p>
+        </div>
+
+        {/* Genres */}
+        {film.genres.length > 0 && (
+          <div className="flex flex-wrap gap-2">
+            {film.genres.map((genre) => (
+              <span
+                key={genre.id}
+                className="bg-secondary text-xs px-3 py-1 rounded-xl"
+              >
+                {genre.name}
+              </span>
+            ))}
+          </div>
+        )}
+
+        {/* Other */}
+        <div className="grid grid-cols-2 gap-3 text-xs text-muted-foreground">
+          {/* Studios */}
+          {film.studios.length > 0 && (
+            <div className="space-y-1">
+              <p className="text-muted-foreground/65">Studios</p>
+              <p>{film.studios.map((studio) => studio.name).join(', ')}</p>
+            </div>
+          )}
+
+          {/* Countries */}
+          {film.countries.length > 0 && (
+            <div className="space-y-1">
+              <p className="text-muted-foreground/65">Countries</p>
+              <p>{film.countries.map((country) => country.name).join(', ')}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </article>
   );
 };
--- src/components/series/card/SeriesCard.tsx
+++ src/components/series/card/SeriesCard.tsx
@@ -1,60 +1,106 @@
-import { ScrollBar, ScrollArea } from '@components/ui/scroll-area';
 import { SeriesListItemFragment } from '@lib/graphql/generated/graphql';
 import { ISOPeriodToLocale } from '@lib/utils/format';
 import { Star } from 'lucide-react';
 import Image from 'next/image';
-import Link from 'next/link';
 
 type Props = {
   series: SeriesListItemFragment;
 };
 
 const SeriesCard = ({ series }: Props) => {
-  const tags = [...series.genres, ...series.countries, ...series.studios];
+  const releasePeriod = ISOPeriodToLocale(
+    series.startReleaseDate,
+    series.endReleaseDate,
+  );
 
   return (
-    <article className="flex flex-col rounded-lg overflow-hidden border h-full divide-y">
-      <div className="relative w-full aspect-video">
-        {series.cover?.url && (
+    <article className="bg-card text-card-foreground rounded-xl shadow-md overflow-hidden border flex flex-col">
+      {/* Main info */}
+      <div className="relative aspect-video">
+        {/* Cover */}
+        {series.cover && (
           <Image
-            src={series.cover.url}
-            fill
             className="object-cover"
-            alt={'cover'}
+            fill
+            src={series.cover?.url}
+            alt={`Cover ${series.title}`}
           />
         )}
-        <div className="absolute top-0 left-0 bg-primary text-sm font-bold rounded-br-md px-3 py-1 text-primary-foreground drop-shadow-lg">
-          {series.ageRestriction}
-        </div>
-        <div className="absolute bottom-0 left-0 bg-chart-1 text-sm font-bold rounded-tr-md px-3 py-1 text-primary-foreground drop-shadow-lg">
-          series
-        </div>
-        <div className="absolute bottom-0 right-0 bg-chart-3 text-sm font-bold rounded-tl-md px-3 py-1 text-primary-foreground drop-shadow-lg flex gap-2 items-center">
-          {series.rating || '-'} <Star className="size-4 fill-current" />
-        </div>
-      </div>
-      <div className="p-4 space-y-2 flex flex-col flex-1">
-        <Link className="w-fit" href={`/series/${series.id}`}>
-          <h2 className="text-xl font-semibold truncate">{series.title}</h2>
-        </Link>
-        <div className="line-clamp-4 text-sm">{series.description}</div>
-        <div className="font-bold text-sm mt-auto">
-          {ISOPeriodToLocale(series.startReleaseDate, series.endReleaseDate)}
-        </div>
-      </div>
-      <ScrollArea className="w-full border-t scroll-p-3">
-        <div className="flex space-x-1.5 p-3 w-max h-12">
-          {tags.map((tag) => (
-            <div
-              key={tag.id}
-              className="bg-secondary text-sm rounded py-0.5 px-2 shrink-0 text-secondary-foreground snap-start"
-            >
-              {tag.name}
+        <div className="absolute inset-0 bg-gradient-to-b via-transparent via-45% to-card"></div>
+
+        {/* Information over the cover */}
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between p-6">
+          <div className="flex justify-between items-start">
+            {/* Rating */}
+            <div className="flex items-center gap-1 px-2 py-1 bg-yellow-500/40  text-primary-foreground backdrop-blur rounded-md font-semibold text-sm">
+              <Star className="text-yellow-500" fill="currentColor" size={16} />
+              {series.rating === 0 ? '-' : series.rating.toFixed(1)}
             </div>
-          ))}
+
+            {/* Age restriction */}
+            <div className="text-sm font-semibold bg-red-600/60 text-primary-foreground rounded-md backdrop-blur px-2 py-1">
+              {series.ageRestriction}
+            </div>
+          </div>
+
+          <div>
+            {/* Title */}
+            <h2 className="text-2xl font-bold">{series.title}</h2>
+
+            {/* Release date */}
+            {releasePeriod && (
+              <div className="text-muted-foreground text-sm">
+                {releasePeriod}
+              </div>
+            )}
+          </div>
         </div>
-        <ScrollBar orientation="horizontal" />
-      </ScrollArea>
+      </div>
+
+      {/* Other info*/}
+      <div className="px-6 py-4 space-y-4 flex-1 flex flex-col">
+        {/* Description */}
+        <div className=" flex-1">
+          <p className="text-muted-foreground text-sm line-clamp-4">
+            {series.description}
+          </p>
+        </div>
+
+        {/* Genres */}
+        {series.genres.length > 0 && (
+          <div className="flex flex-wrap gap-2">
+            {series.genres.map((genre) => (
+              <span
+                key={genre.id}
+                className="bg-secondary text-xs px-3 py-1 rounded-xl"
+              >
+                {genre.name}
+              </span>
+            ))}
+          </div>
+        )}
+
+        {/* Other */}
+        <div className="grid grid-cols-2 gap-3 text-xs text-muted-foreground">
+          {/* Studios */}
+          {series.studios.length > 0 && (
+            <div className="space-y-1">
+              <p className="text-muted-foreground/65">Studios</p>
+              <p>{series.studios.map((studio) => studio.name).join(', ')}</p>
+            </div>
+          )}
+
+          {/* Countries */}
+          {series.countries.length > 0 && (
+            <div className="space-y-1">
+              <p className="text-muted-foreground/65">Countries</p>
+              <p>
+                {series.countries.map((country) => country.name).join(', ')}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
     </article>
   );
 };
