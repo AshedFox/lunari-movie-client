c34df34d982ace7af54e589a03a206abe66b0836
AshedFox <a.mahn@mail.ru>
2025-03-20 16:15:55 +0300

    add auth modals

---
+++ src/app/@auth/(.)login/page.tsx
@@ -0,0 +1,26 @@
+import LoginForm from '@app/(guest)/login/_components/LoginForm';
+import NextModal from '@components/common/NextModal';
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@components/ui/dialog';
+
+const Page = () => {
+  return (
+    <NextModal>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="sr-only">Login</DialogTitle>
+          <DialogDescription className="sr-only">
+            Login to the account
+          </DialogDescription>
+        </DialogHeader>
+        <LoginForm />
+      </DialogContent>
+    </NextModal>
+  );
+};
+
+export default Page;
