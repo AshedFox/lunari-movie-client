1348d92ff924a823efc94526ae4e49da021c8735
AshedFox <a.mahn@mail.ru>
2025-04-05 00:11:41 +0300

    replace consant gql schema path with env var

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:3000/graphql',
+  schema: process.env.SCHEMA_PATH,
   documents: ['src/**/*.{tsx,ts,graphql}'],
   ignoreNoDocuments: true,
   generates: {
