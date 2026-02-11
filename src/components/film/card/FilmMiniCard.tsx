1898e88ccdd54e36057f902d6350ccd45e059f19
AshedFox <a.mahn@mail.ru>
2026-02-11 11:32:43 +0300

    feat: restyle film and series mini cards

--- src/components/film/card/FilmMiniCard.tsx
+++ src/components/film/card/FilmMiniCard.tsx
@@ -1,7 +1,6 @@
-'use client';
-
+import { Badge } from '@components/ui/badge';
+import { ScrollArea, ScrollBar } from '@components/ui/scroll-area';
 import { FilmMiniCardFragment } from '@lib/graphql/generated/graphql';
-import { cn } from '@lib/utils';
 import { formatDateTime } from '@lib/utils/format';
 import { Film, Star } from 'lucide-react';
 import Image from 'next/image';
@@ -15,88 +14,63 @@ const FilmMiniCard = ({ film }: Props) => {
   const releaseDate = film.releaseDate && formatDateTime(film.releaseDate);
 
   return (
-    <Link href={`/films/${film.id}`}>
-      <article className="bg-card text-card-foreground rounded-xl shadow-md overflow-hidden border flex flex-col group">
-        {/* Main info */}
-        <div className="relative aspect-video">
-          {/* Cover */}
-          {film.cover && (
-            <Image
-              className="object-cover"
-              fill
-              src={film.cover?.url}
-              alt={`Cover ${film.title}`}
-            />
-          )}
-          <div
-            className={cn('absolute inset-0', {
-              'bg-gradient-to-b via-transparent via-45% to-card group-hover:via-card/50 transition-colors duration-300':
-                film.cover,
-              'bg-muted/50': !film.cover,
-            })}
+    <div className="bg-card text-card-foreground rounded-xl shadow-md overflow-hidden border flex flex-col">
+      <div className="relative aspect-square overflow-hidden bg-muted">
+        {film.cover ? (
+          <Image
+            src={film.cover.url}
+            alt={film.title}
+            fill
+            className="object-cover "
+            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 20vw"
           />
-
-          {/* Information over the cover */}
-          <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between p-6">
-            <div className="flex justify-between items-start">
-              {/* Rating */}
-              <div className="flex items-center gap-1 px-2 py-1 bg-yellow-500/40 text-primary-foreground backdrop-blur rounded-md font-semibold text-sm">
-                <Star
-                  className="text-yellow-500"
-                  fill="currentColor"
-                  size={16}
-                />
-                {film.rating === 0 ? '-' : film.rating.toFixed(1)}
-              </div>
-
-              {/* Age restriction */}
-              <div className="text-sm font-semibold bg-red-600/60 text-primary-foreground rounded-md backdrop-blur px-2 py-1">
-                {film.ageRestriction}
-              </div>
-            </div>
-
-            <div>
-              {/* Title */}
-              <div
-                className={cn(
-                  'flex justify-between transition-transform group-hover:translate-y-0 duration-300',
-                  film.genres.length > 0
-                    ? 'translate-y-[150%]'
-                    : 'translate-y-[50%]',
-                )}
-              >
-                <h2 className={'text-2xl font-bold w-fit'}>{film.title}</h2>
-                <div title="series">
-                  <Film size={24} />
-                </div>
-              </div>
-              <div className="transition-transform translate-y-[200%] group-hover:translate-y-0 duration-300">
-                {/* Release date */}
-                {releaseDate && (
-                  <div className="text-muted-foreground text-sm">
-                    {releaseDate}
-                  </div>
-                )}
-
-                {/* Genres */}
-                {film.genres.length > 0 && (
-                  <div className="flex flex-wrap gap-2 mt-2">
-                    {film.genres.map((genre) => (
-                      <span
-                        key={genre.id}
-                        className="bg-secondary text-xs px-3 py-1 rounded-xl"
-                      >
-                        {genre.name}
-                      </span>
-                    ))}
-                  </div>
-                )}
-              </div>
-            </div>
+        ) : (
+          <div className="flex h-full items-center justify-center">
+            <Film className="h-10 w-10 text-muted-foreground" />
           </div>
+        )}
+
+        {/* Badges on top of image */}
+        <div className="absolute top-0 p-2 w-full flex justify-between">
+          {/* Rating */}
+          {!!film.rating && (
+            <Badge className="bg-yellow-500/40">
+              <Star className="text-yellow-500" fill="currentColor" size={16} />
+              {film.rating.toFixed(1)}
+            </Badge>
+          )}
+
+          {/* Age restriction */}
+          <Badge className="ml-auto bg-red-600/60">{film.ageRestriction}</Badge>
         </div>
-      </article>
-    </Link>
+      </div>
+
+      <div className="px-6 py-4 space-y-4 flex-1 flex flex-col">
+        <h3
+          className="font-medium group-hover:text-primary transition-colors line-clamp-1"
+          title={film.title}
+        >
+          <Link href={`/films/${film.id}`}>{film.title}</Link>
+        </h3>
+
+        {releaseDate && (
+          <div className="text-muted-foreground text-sm">{releaseDate}</div>
+        )}
+
+        {film.genres.length > 0 && (
+          <ScrollArea>
+            <div className="flex gap-1">
+              {film.genres.map((genre) => (
+                <Badge key={genre.id} variant="secondary">
+                  {genre.name}
+                </Badge>
+              ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        )}
+      </div>
+    </div>
   );
 };
 
--- src/components/series/card/SeriesMiniCard.tsx
+++ src/components/series/card/SeriesMiniCard.tsx
@@ -1,5 +1,6 @@
+import { Badge } from '@components/ui/badge';
+import { ScrollArea, ScrollBar } from '@components/ui/scroll-area';
 import { SeriesMiniCardFragment } from '@lib/graphql/generated/graphql';
-import { cn } from '@lib/utils';
 import { formatDateTimeRange } from '@lib/utils/format';
 import { Star, Tv } from 'lucide-react';
 import Image from 'next/image';
@@ -16,89 +17,65 @@ const SeriesMiniCard = ({ series }: Props) => {
   );
 
   return (
-    <Link href={`/series/${series.id}`}>
-      <article className="bg-card text-card-foreground rounded-xl shadow-md overflow-hidden border flex flex-col group">
-        {/* Main info */}
-        <div className="relative aspect-video">
-          {/* Cover */}
-          {series.cover && (
-            <Image
-              className="object-cover"
-              fill
-              src={series.cover?.url}
-              alt={`Cover ${series.title}`}
-            />
-          )}
-          <div
-            className={cn('absolute inset-0', {
-              'bg-gradient-to-b via-transparent via-45% to-card group-hover:via-card/50 transition-colors duration-300':
-                series.cover,
-              'bg-muted/50': !series.cover,
-            })}
+    <div className="bg-card text-card-foreground rounded-xl shadow-md overflow-hidden border flex flex-col">
+      <div className="relative aspect-square overflow-hidden bg-muted">
+        {series.cover ? (
+          <Image
+            src={series.cover.url}
+            alt={series.title}
+            fill
+            className="object-cover"
+            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 20vw"
           />
-
-          {/* Information over the cover */}
-          <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between p-6">
-            <div className="flex justify-between items-start">
-              {/* Rating */}
-              <div className="flex items-center gap-1 px-2 py-1 bg-yellow-500/40 text-primary-foreground backdrop-blur rounded-md font-semibold text-sm">
-                <Star
-                  className="text-yellow-500"
-                  fill="currentColor"
-                  size={16}
-                />
-                {series.rating === 0 ? '-' : series.rating.toFixed(1)}
-              </div>
-
-              {/* Age restriction */}
-              <div className="text-sm font-semibold bg-red-600/60 text-primary-foreground rounded-md backdrop-blur px-2 py-1">
-                {series.ageRestriction}
-              </div>
-            </div>
-
-            <div>
-              {/* Title */}
-              <div
-                className={cn(
-                  'flex justify-between transition-transform group-hover:translate-y-0 duration-300',
-                  series.genres.length > 0
-                    ? 'translate-y-[150%]'
-                    : 'translate-y-[50%]',
-                )}
-              >
-                <h2 className={'text-2xl font-bold w-fit'}>{series.title}</h2>
-                <div title="series">
-                  <Tv size={24} />
-                </div>
-              </div>
-
-              <div className="transition-transform translate-y-[200%] group-hover:translate-y-0 duration-300">
-                {/* Release date */}
-                {releasePeriod && (
-                  <div className="text-muted-foreground text-sm">
-                    {releasePeriod}
-                  </div>
-                )}
-
-                {/* Genres */}
-                {series.genres.length > 0 && (
-                  <div className="flex flex-wrap gap-2 mt-2">
-                    {series.genres.map((genre) => (
-                      <span
-                        key={genre.id}
-                        className="bg-secondary text-xs px-3 py-1 rounded-xl"
-                      >
-                        {genre.name}
-                      </span>
-                    ))}
-                  </div>
-                )}
-              </div>
-            </div>
+        ) : (
+          <div className="flex h-full items-center justify-center">
+            <Tv className="h-10 w-10 text-muted-foreground" />
           </div>
+        )}
+
+        {/* Badges on top of image */}
+        <div className="absolute top-0 p-2 w-full flex justify-between">
+          {/* Rating */}
+          {!!series.rating && (
+            <Badge className="bg-yellow-500/40">
+              <Star className="text-yellow-500" fill="currentColor" size={16} />
+              {series.rating.toFixed(1)}
+            </Badge>
+          )}
+
+          {/* Age restriction */}
+          <Badge className="ml-auto bg-red-600/60">
+            {series.ageRestriction}
+          </Badge>
         </div>
-      </article>
-    </Link>
+      </div>
+
+      <div className="px-6 py-4 space-y-4 flex-1 flex flex-col">
+        <h3
+          className="font-medium group-hover:text-primary transition-colors line-clamp-1"
+          title={series.title}
+        >
+          <Link href={`/series/${series.id}`}>{series.title}</Link>
+        </h3>
+
+        {releasePeriod && (
+          <div className="text-muted-foreground text-sm">{releasePeriod}</div>
+        )}
+
+        {series.genres.length > 0 && (
+          <ScrollArea>
+            <div className="flex gap-1">
+              {series.genres.map((genre) => (
+                <Badge key={genre.id} variant="secondary">
+                  {genre.name}
+                </Badge>
+              ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        )}
+      </div>
+    </div>
   );
 };
 
