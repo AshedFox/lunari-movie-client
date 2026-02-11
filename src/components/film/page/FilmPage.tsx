048a63b3e2545b936b683a5756351770cbc86f90
AshedFox <a.mahn@mail.ru>
2026-02-11 16:06:12 +0300

    feat: restyle film page

--- src/components/film/page/FilmPage.tsx
+++ src/components/film/page/FilmPage.tsx
@@ -8,6 +8,7 @@ import { formatDateTime } from '@lib/utils/format';
 import Image from 'next/image';
 import { MovieListsButtons } from '@components/movie-user/lists-buttons';
 import WatchButton from './WatchButton';
+import { Badge } from '@components/ui/badge';
 
 type Props = {
   film: FilmFragment;
@@ -19,55 +20,50 @@ const FilmPage = ({ film, movieUser, user }: Props) => {
   return (
     <div className="space-y-8">
       {/* Cover */}
-      {film.cover && (
-        <div className="w-full aspect-[3/1] relative">
+      <div className="w-full aspect-[3/1] relative bg-muted">
+        {film.cover && (
           <Image
             src={film.cover.url}
             alt="cover"
             fill
             className="object-cover"
           />
-          <div className="absolute inset-0 bg-gradient-to-b via-transparent via-60% to-background/50" />
-        </div>
-      )}
+        )}
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-background" />
+      </div>
 
       {/* Content */}
       <div className="relative container space-y-6">
         {/* Title */}
         <h1 className="text-5xl font-bold leading-tight">{film.title}</h1>
-        <div className="flex flex-wrap items-center gap-6">
+        <div className="flex flex-wrap items-center gap-3">
           {/* Rating */}
-          <div className="flex items-center gap-2">
-            <Star className="text-yellow-500" fill="currentColor" size={24} />
-            <span className="text-2xl font-bold">
-              {film.rating === 0 ? '-' : film.rating.toFixed(1)}
-            </span>
-          </div>
+          <Badge className="px-3 py-2 bg-yellow-500">
+            <Star fill="currentColor" />
+            <span>{film.rating === 0 ? '-' : film.rating.toFixed(1)}</span>
+          </Badge>
 
           {/* Release date */}
           {film.releaseDate && (
-            <div className="flex items-center gap-2 text-muted-foreground text-sm font-semibold">
-              <Calendar size={20} />
+            <Badge variant="outline" className="px-3 py-2">
+              <Calendar />
               <span>{formatDateTime(film.releaseDate)}</span>
-            </div>
+            </Badge>
           )}
 
           {film.ageRestriction && (
-            <span className="px-3 py-1 bg-red-600 text-primary-foreground font-semibold rounded-md text-sm">
+            <Badge className="px-3 py-2 bg-red-600">
               {film.ageRestriction}
-            </span>
+            </Badge>
           )}
         </div>
 
         {/* Genres */}
         <div className="flex flex-wrap gap-2 mb-6">
           {film.genres.map((genre) => (
-            <span
-              key={genre.id}
-              className="bg-secondary text-xs px-3 py-1 rounded-xl"
-            >
+            <Badge variant="secondary" key={genre.id}>
               {genre.name}
-            </span>
+            </Badge>
           ))}
         </div>
 
