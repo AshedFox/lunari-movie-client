c2a261ceca220e9678db48556473840ea85a707b
AshedFox <a.mahn@mail.ru>
2026-02-13 12:43:15 +0300

    refactor: use http-link instead of upload link in rsc apollo client to avoid issues

--- src/lib/apollo/rsc-client.ts
+++ src/lib/apollo/rsc-client.ts
@@ -5,22 +5,21 @@ import {
   InMemoryCache,
   registerApolloClient,
 } from '@apollo/client-integration-nextjs';
-import { ApolloLink } from '@apollo/client';
+import { ApolloLink, HttpLink } from '@apollo/client';
 import { removeTypenameLink } from '@lib/apollo/remove-typename-link';
 import { authLink } from '@lib/apollo/rsc-auth-link';
 import { env } from '@lib/env/server';
-import UploadHttpLink from 'apollo-upload-client/UploadHttpLink.mjs';
 
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
-  const uploadLink = new UploadHttpLink({
+  const httpLink = new HttpLink({
     uri: `${env.NEXT_PUBLIC_API_URL}/graphql`,
     headers: {
       'Apollo-Require-Preflight': 'true',
     },
-  }) as ApolloLink;
+  });
 
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: ApolloLink.from([removeTypenameLink, authLink, uploadLink]),
+    link: ApolloLink.from([removeTypenameLink, authLink, httpLink]),
   });
 });
