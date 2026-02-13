e65f195b696afc9c2bd4ea513135db032e681d64
AshedFox <a.mahn@mail.ru>
2026-02-13 12:46:07 +0300

    refactor: use @container queries on guest pages

--- src/app/(guest)/reset-password/[token]/page.tsx
+++ src/app/(guest)/reset-password/[token]/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import ResetPasswordForm from './_components/ResetPasswordForm';
-import BgImage from '../../../../../public/image.png';
 import Image from 'next/image';
 
 export const metadata: Metadata = {
@@ -17,23 +16,25 @@ const Page = async ({ params }: Props) => {
   const { token } = await params;
 
   return (
-    <div className="grid lg:grid-cols-2">
-      <div className="flex flex-1 items-center justify-center p-6 md:p-10">
-        <div className="w-full max-w-sm">
-          <ResetPasswordForm token={token} />
+    <div className="@container">
+      <div className="grid @lg:grid-cols-2">
+        <div className="flex flex-1 items-center justify-center p-6 @md:p-10">
+          <div className="w-full max-w-sm">
+            <ResetPasswordForm token={token} />
+          </div>
         </div>
-      </div>
 
-      <div className="relative hidden bg-muted lg:block">
-        <Image
-          src={BgImage}
-          alt="Auth"
-          placeholder="blur"
-          quality={100}
-          fill
-          sizes="50vw"
-          className="object-cover grayscale-50 brightness-75 dark:brightness-50 dark:grayscale-75"
-        />
+        <div className="relative hidden bg-muted @lg:block">
+          <Image
+            src="/image.png"
+            alt="Auth"
+            placeholder="blur"
+            quality={100}
+            fill
+            sizes="50vw"
+            className="object-cover grayscale-50 brightness-75 dark:brightness-50 dark:grayscale-75"
+          />
+        </div>
       </div>
     </div>
   );
