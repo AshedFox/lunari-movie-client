bd9eebe61d6f989f026d478c945568a62910122d
AshedFox <a.mahn@mail.ru>
2026-02-17 11:24:55 +0300

    refactor: allow custom format pattern in FormattedDateRange

--- src/shared/ui/formatted-date-range.tsx
+++ src/shared/ui/formatted-date-range.tsx
@@ -2,7 +2,7 @@ import {
   DATE_FORMATS,
   DateTimeFormat,
   DateTimeVariant,
-} from '@shared/lib/format/formatters';
+} from '@shared/lib/format';
 import { format as formatFn } from 'date-fns';
 import { cn } from '@shared/lib/utils';
 import { HTMLAttributes } from 'react';
@@ -11,7 +11,7 @@ interface FormattedDateRangeProps extends HTMLAttributes<HTMLSpanElement> {
   fromDate?: Date | string | number | null;
   toDate?: Date | string | number | null;
   variant?: DateTimeVariant;
-  format?: DateTimeFormat;
+  format?: DateTimeFormat | ({} & string);
 }
 
 export function FormattedDateRange({
@@ -22,7 +22,10 @@ export function FormattedDateRange({
   className,
   ...props
 }: FormattedDateRangeProps) {
-  const pattern = DATE_FORMATS[variant][format];
+  const pattern =
+    format in DATE_FORMATS[variant]
+      ? DATE_FORMATS[variant][format as DateTimeFormat]
+      : format;
 
   const formattedStart = fromDate ? formatFn(new Date(fromDate), pattern) : '…';
   const formattedEnd = toDate ? formatFn(new Date(toDate), pattern) : '…';
